f9e60e114d45e6c769122634a03574c82e5e373a
Newton Muriithi <newtonMM>
2022-07-28 16:12:12 +0300

    project setup

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=Newty
+export POSTGRES_PASSWORD=Newty2022
+export POSTGRES_HOST=project3-db.ctqxy52aj71r.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project3_db
+export AWS_BUCKET=arn:aws:s3:::project3-243248059576-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
