f18a6329553ea7f3b5c27d96b6f9752f15195f75
Newton Muriithi <newtonMM>
2022-08-14 16:18:04 +0300

    environment variables update

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=Newty
 export POSTGRES_PASSWORD=Newty2022
-export POSTGRES_HOST=project3-db.ctqxy52aj71r.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=project3_db
-export AWS_BUCKET=arn:aws:s3:::project3-243248059576-dev
+export POSTGRES_HOST=project3.cz9rslrthci0.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=prj3-850971606778-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
