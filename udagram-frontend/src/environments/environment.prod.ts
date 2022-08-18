d1d88eacc645f15f9dce064a95667935b13a1b22
Newton Muriithi <newtonMM>
2022-08-18 21:02:43 +0300

    orchestration with kubernetes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    'http://a3c5386b566f1463bb5cedf160e248e2-1573580904.us-east-2.elb.amazonaws.com:8080/api/v0"',
 };
 
 /*
